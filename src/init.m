366696bcea7cfdda63ddecedd92755967ed62470
Laufinsconsca <walle.log@yandex.ru>
2019-07-10 16:39:30 +0400

    10-07-2019

--- src/init.m
+++ src/init.m
@@ -3,7 +3,9 @@ n = 800; % size of image
 pow_fi = 2; % power of fi in exp(i*tp*fi)
 tp = 5; % topological charge (for independent topological charge)
 % ----------------------------------------------------------
+% if the count of input arguments is 4 then tp = const, else if 3 then tp = tp(r)
 vortex = vortex_filter(r, n, pow_fi);
+% ----------------------------------------------------------
 vortex = 255*angle(vortex)/(2*pi);
 for j=1:size(vortex,1)
     for k=1:size(vortex,2)
